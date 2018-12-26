da05c873aeab72c9efe531b2c1fd3d6d64254707
Big Mac <ayoub.belhadji@gmail.com>
2018-12-26 23:13:58 +0100

    remove redundant tests

--- CSSPy/largest_leveragescores_sampler.py
+++ CSSPy/largest_leveragescores_sampler.py
@@ -17,17 +17,13 @@ from scipy.sparse.linalg import svds
 class largest_leveragescores_Sampler:
     def __init__(self, A, k, Q,N):
         self.A = A
-
         self.Q = np.transpose(Q[0:k,:])
         self.N = N
         self.k = k
         self.sampling_list = []
-        self.column_selected_temp = np.zeros(k)
         self.lvs_array = self.Estimate_Leverage_Scores()
-
     def Estimate_Leverage_Scores(self):
         return 1/(self.k)*np.diag(np.dot(self.Q,self.Q.T))
-
     def MultiRounds(self):
         temp_list = list(reversed(np.argsort(self.lvs_array)))
         sampled_indices_ = temp_list[0:self.k]
