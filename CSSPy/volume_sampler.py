4dad56e5e569ec1496f83f980f1bc4d4ff225182
AyoubBelhadji <ayoub.belhadji@gmail.com>
2019-01-25 11:19:35 +0100

    Update volume_sampler.py

--- CSSPy/volume_sampler.py
+++ CSSPy/volume_sampler.py
@@ -26,11 +26,8 @@ class k_Volume_Sampling_Sampler:
         self.column_selected_temp = np.zeros(k)
     def OneRound(self):
         sampled_indices = np.random.choice(self.N, 1, replace=True, p=list(self.lvs_array))
-        #self.sampling_list.append(sampled_indices)
         column_selected = self.Q_temp[:,sampled_indices[0]]
-        #print(np.linalg.norm(column_selected))
         self.column_selected_temp = 1/np.linalg.norm(column_selected)*column_selected
-        #print(np.linalg.norm(self.column_selected_temp))
         self.Project_On_The_Vector_Orthogonal()
         self.sampling_round = self.sampling_round +1
         return sampled_indices[0]
@@ -40,74 +37,23 @@ class k_Volume_Sampling_Sampler:
 
 
 def k_dpp_KuTa12(eig_val, eig_vec, k):
-    """ Sample from :math:`\operatorname{k-DPP}(K)` using the eigen-decomposition of the
-    similarity kernel :math:`K`.
-    :param eig_val: 
-        Collection of eigen values of the similarity kernel :math:`K`.
-    :type eig_val: 
-        list
-    :param eig_vec: 
-        Eigen-vectors of the similarity kernel :math:`K`.
-    :type eig_vec: 
-        array_type
-    
-    :param k: 
-        Size of the sample.
-    :type k: 
-        int
-    :return: 
-        A sample from :math:`\operatorname{k-DPP}(K)`.
-    :rtype: 
-        list
-            
-    .. seealso::
-        Algorithm 1, 8 in :cite:`KuTa12` 
-        - :func:`dpp_exact_sampling_KuTa12 <dpp_exact_sampling_KuTa12>`
-        - :func:`k_dpp_TrBaAm17_Wood <k_dpp_TrBaAm17_Wood>`
-        - :func:`k_dpp_TrBaAm17_Fs <k_dpp_TrBaAm17_Fs>`
-    """
-
     E = elem_symm_poly(eig_val, k) # Evaluate the elem symm polys in the eig_values 
     N = len(eig_val) # Size of the ground set
     S = [] # Initialization of the output
     l = k # Size of the sample
-
     for n in range(N,0,-1):
         if l == 0:
             break
         if np.random.rand() < eig_val[n-1]*(E[l-1,n-1]/E[l,n]):
             S.append(n-1)
             l-=1
-
     eig_v = np.zeros(N)
     eig_v[S] = 1.0
-
     return dpp_exact_sampling_KuTa12(eig_v, eig_vec)
 
 
 
 def elem_symm_poly(eig_val, k):
-    """ Evaluate the elementary symmetric polynomials in the 
-    eigen-values of the similarity kernel :math:`K`.
-    :param eig_val: 
-        Collection of eigen values of the similarity kernel :math:`K`.
-    :type eig_val: 
-        list
-    
-    :param k: 
-        Maximum degree of elementary symmetric polynomial.
-    :type k: 
-        int
-            
-    :return: 
-        poly(k,N) = :math:`e_k(\lambda_1, \cdots, \lambda_N)`
-    :rtype: 
-        array_type
-    .. seealso::
-        Algorithm 7 in :cite:`KuTa12`
-        - :func:`k_dpp_KuTa12 <k_dpp_KuTa12>`
-    """
-
     # Number of variables for the elementary symmetric polynomials to be evaluated
     N = len(eig_val)
     # Initialize output array
@@ -123,28 +69,6 @@ def elem_symm_poly(eig_val, k):
 
 
 def dpp_exact_sampling_KuTa12(eig_val, eig_vec):
-    """ Sample from :math:`\operatorname{DPP}(K)` using the eigen-decomposition of the
-    similarity kernel :math:`K`. 
-    It is based on the orthogonalization of the selected eigen-vectors.
-    :param eig_val: 
-        Collection of eigen values of the similarity kernel :math:`K`.
-    :type eig_val: 
-        list
-    :param eig_vec: 
-        Eigen-vectors of the similarity kernel :math:`K`.
-    :type eig_vec: 
-        array_type
-        
-    :return: 
-        A sample from :math:`\operatorname{DPP}(K)`.
-    :rtype: 
-        list
-    .. seealso::
-        Algorithm 1 in :cite:`KuTa12`
-        - :func:`dpp_exact_sampling_Wood <dpp_exact_sampling_Wood>`
-        - :func:`dpp_exact_sampling_GS <dpp_exact_sampling_GS>`
-    """
-
     # Phase 1: 
     # Select eigen vectors \propto eig_vals
     ind_bool = np.random.rand(len(eig_val)) < eig_val
